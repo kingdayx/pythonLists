c8b52c7dc53789643568f5afedfde01c7c9d1f39
kingdayx <54965488+kingdayx@users.noreply.github.com>
2019-09-16 13:49:47 -0400

    added a funtion and else if
    added a function to show what the user neeeds to do with the loop. added elif to  cause the function to print to screen if help if typed by user

--- lists.py
+++ lists.py
@@ -1,5 +1,15 @@
+def show_help():
+    print("""
+    Enter 'DONE' to finish adding items
+    Enter 'HELP' to bring up help
+    """)
 
+
+show_help()
 while True:
     new_item = input("> ")
     if new_item == 'DONE':
         break
+    elif new_item == 'HELP':
+        show_help()
+        continue
