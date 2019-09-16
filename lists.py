746d8ebc8c2c640c95d84688839f28835828b5a1
kingdayx <54965488+kingdayx@users.noreply.github.com>
2019-09-16 14:12:12 -0400

    added the list and a function
    added the shopping list and a function that adds items to the list and tells the user that the item was added to the list and the amount of items in that list. called the function at the end of the list with the user's input as the parameter

--- lists.py
+++ lists.py
@@ -1,3 +1,11 @@
+shopping_list = []
+
+
+def add_to_list(item):
+    shopping_list.append(item)
+    print("The item was added! There are now {} items in the list!".format(len(shopping_list)))
+
+
 def show_help():
     print("""
     Enter 'DONE' to finish adding items
@@ -13,3 +21,6 @@ while True:
     elif new_item == 'HELP':
         show_help()
         continue
+    # call add_to_list with new_item as argument
+    add_to_list(new_item)
+
